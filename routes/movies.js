2ca8ecf6051c3c29b1cb8d224030fdfbe8044b3f
Benjamin Levaltier <benji1705@hotmail.fr>
2021-11-03 22:43:46 +0100

    Test PUT impossible

--- routes/movies.js
+++ routes/movies.js
@@ -1,23 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+let movies= [{
+  movies :"SW1",
+  id : "0"
+}];
+
 /* GET movies listing. */
-router.get('/', function(req, res, next) {
-  
-  res.status(200).json({success,
-  movie
+router.get('/', function (req, res, next) {
+
+  res.status(200).json({movies
   });
 });
 
 module.exports = router;
 
 /*PUT new movies*/
-router.put('/',function(req, res, next){
-    const {movies} = req.body;
-    const id = _.unique();
-    movies.push({movies,id});
-    res.json({
-        message: `ça marche frère = ${id}`,
-        movies:{movies,id}
-    });
+router.put('/', function (req, res, next) {
+  const { movie } = req.body;
+  const id = _.uniqueId();
+  movies.push({ movie, id });
+  res.json({
+    message: `Done = ${id}`,
+    movies: { movie, id } 
+  });
 });
