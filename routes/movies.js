a0b38800ec2ef5a1dfae19d6a4fcca50a30c1335
Benjamin Levaltier <benji1705@hotmail.fr>
2021-11-03 19:58:31 +0100

    Test

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET movies listing. */
 router.get('/', function(req, res, next) {
-  res.send('YES');
+  
+  res.status(200).json({success,
+  movie
+  });
 });
 
 module.exports = router;
@@ -12,7 +15,7 @@ module.exports = router;
 router.put('/',function(req, res, next){
     const {movies} = req.body;
     const id = _.unique();
-    movies.push({movie,id});
+    movies.push({movies,id});
     res.json({
         message: `ça marche frère = ${id}`,
         movies:{movies,id}
